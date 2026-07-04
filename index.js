5bb75f03a9d849e5054c16c709299e3ea6c3426f
Sandeep21803 <sranjeetkumar995@gmail.com>
2026-07-04 19:16:20 +0000

    Jul 5, 2026, 12:46 AM

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require('express');
 const app = express()
-const PORT =3000 
+const port =process.env.PORT || 3000 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -11,6 +12,6 @@ app.get('/twitter',(req,res)=>{
 app.get('/login',(req,res)=>{
     res.send('<h1>Please login at my site</h1>')
 })
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
